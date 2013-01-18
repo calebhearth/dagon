ba91c610b8ee2c7600e72a4425c2032973667b6a
halogenandtoast <halogenandtoast@gmail.com>
2013-01-18 00:17:11 -0500

    Undo debug.

--- lib/dagon/interpreter/identifier.rb
+++ lib/dagon/interpreter/identifier.rb
@@ -8,19 +8,13 @@ module Dagon
       binding.lookup(to_sym)
     end
     def parse
-      if (node = next_node) != node_name
+      if (node = next_node) != :identifier
         error "Invalid identifier #{node}"
       end
       @name = next_node
     end
     def to_sym
       @name.to_sym
-    end
-    def node_name
-      :identifier
-    end
-    def to_s
-      to_sym
     end
   end
 end
